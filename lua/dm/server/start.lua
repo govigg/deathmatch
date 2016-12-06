0588e87f219bd5b959c22b6bcedb11058e376f5b
Fred <hupobb1@o2.pl>
2016-12-06 18:39:39 +0100

    Spawny

--- lua/dm/server/start.lua
+++ lua/dm/server/start.lua
@@ -1,4 +1,18 @@
-
 hook.Add("MinigamesGameStarted","testowy",function()
-	print("OK")
+	if Minigames.ActualGame == "deathmatch" then
+		startFirstDM()
+	end
 end)
+
+function startFirstDM()
+	local spawns = Minigames.buildingMode.getActualMinigameSpawns()
+	for i,k in pairs(Minigames.PlayersQue) do
+		local rand = math.Rand(1,table.Count(spawns))
+		rand = math.floor(rand)
+
+		local rSpawn = spawns[rand]
+
+		k:SetPos(rSpawn.pos)
+		k:SetAngles(Angle(0,rSpawn.ang,0))
+	end
+end
